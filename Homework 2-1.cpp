3810a7af8025c67f65a9d6270b29b436da2d2108
AlexanderZmeykin <alexanderlatushev731@gmail.com>
2025-12-16 23:18:35 +0300

    Add files via upload

---
+++ Homework 2-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	int res;
+	std::cout << "Input number:" << std::endl;
+	std::cin >> res;
+	std::cout << "You wrote number: " << res << std::endl;
+
+	return EXIT_SUCCESS;
+}
---
+++ Homework 2-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	std::string res;
+	std::cout << "Input word:" << std::endl;
+	std::cin >> res;
+	std::cout << "You wrote: " << res << std::endl;
+
+	return EXIT_SUCCESS;
+}
